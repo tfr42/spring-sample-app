011b51193899c268ed0caa77cb9e629c2f067750
Rene Gielen <rgielen@apache.org>
2015-12-02 14:26:44 +0100

    Komplettes Spring+JPA+TransactionManager+DataSource Setup

--- src/main/java/net/gfu/seminar/spring/lab/todoapp/TodoAppConfig.java
+++ src/main/java/net/gfu/seminar/spring/lab/todoapp/TodoAppConfig.java
@@ -2,23 +2,31 @@ package net.gfu.seminar.spring.lab.todoapp;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan
+@EnableTransactionManagement
 // Ohne Parameteter gleichbedeutend mit:
 // @ComponentScan(basePackageClasses = {TodoAppConfig.class})
 public class TodoAppConfig {
 
 	@Bean
+	// Auch möglich: @Lazy @Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.h2.Driver");
@@ -38,6 +46,13 @@ public class TodoAppConfig {
 		factory.setJpaVendorAdapter(adapter);
 		factory.setJpaProperties(additionalProperties());
 		return factory;
+	}
+	
+	@Bean @Autowired
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager txm = new JpaTransactionManager();
+		txm.setEntityManagerFactory(emf);
+		return txm;
 	}
 
 	private Properties additionalProperties() {
--- src/main/java/net/gfu/seminar/spring/lab/todoapp/dao/TodoItemDao.java
+++ src/main/java/net/gfu/seminar/spring/lab/todoapp/dao/TodoItemDao.java
@@ -4,10 +4,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import net.gfu.seminar.spring.lab.todoapp.domain.TodoItem;
 
 @Component
+@Transactional
 public class TodoItemDao {
 
 	@PersistenceContext
