d4d78bd91b2222fd3dbd5f25889611306651921f
Rene Gielen <rgielen@apache.org>
2015-12-03 22:07:18 +0100

    Property Sources und Profiles eingeführt

--- src/main/java/net/gfu/seminar/spring/lab/todoapp/TodoAppConfig.java
+++ src/main/java/net/gfu/seminar/spring/lab/todoapp/TodoAppConfig.java
@@ -2,13 +2,20 @@ package net.gfu.seminar.spring.lab.todoapp;
 
 import java.util.Properties;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -22,19 +29,39 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
+@PropertySource("file:${user.dir}/database.properties")
 // Ohne Parameteter gleichbedeutend mit:
 // @ComponentScan(basePackageClasses = {TodoAppConfig.class})
 public class TodoAppConfig {
 
-	@Bean
+	@Autowired
+	private Environment env;
+	
+	@Value("${jdbc.datasource.default.url}")
+	private String jdbcUrl;
+
+	@Value("${jdbc.datasource.default.user}")
+	private String jdbcUser;
+	
+	@Value("${jdbc.datasource.default.password}")
+	private String jdbcPassword;
+	
+	
+	@Bean @Profile({"default", "test"})
 	// Auch möglich: @Lazy @Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		dataSource.setUrl("jdbc:h2:./todo;DB_CLOSE_DELAY=-1;INIT=CREATE SCHEMA IF NOT EXISTS testDatabase");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
+		dataSource.setDriverClassName(env.getProperty("jdbc.datasource.default.driver"));
+		dataSource.setUrl(jdbcUrl);
+		dataSource.setUsername(jdbcUser);
+		dataSource.setPassword(jdbcPassword);
 		return dataSource;
+	}
+	
+	@Bean @Profile({"production"})
+	public DataSource productionDataSource() throws Exception {
+		Context ctx = new InitialContext();
+		return (DataSource) ctx.lookup("java.comp/env/jdbc/todoDataSource");
 	}
 	
 	//@Bean
@@ -46,10 +73,10 @@ public class TodoAppConfig {
             .build();
     }
 	
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	@Bean @Autowired
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
 		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-		factory.setDataSource(dataSource());
+		factory.setDataSource(dataSource);
 		factory.setPackagesToScan("net.gfu.seminar.spring.lab.todoapp.domain");
 		
 		JpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
@@ -65,6 +92,11 @@ public class TodoAppConfig {
 		return txm;
 	}
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+	
 	private Properties additionalProperties() {
 		Properties prop = new Properties();
         prop.setProperty("hibernate.hbm2ddl.auto", "update");
