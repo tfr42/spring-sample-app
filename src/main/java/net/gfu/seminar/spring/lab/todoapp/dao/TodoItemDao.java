677423598932d20c53bde7af5e68921aa1cc0d0d
Rene Gielen <rgielen@apache.org>
2015-12-03 22:16:41 +0100

    Verdeutlichung von @Transactional Aspekt

--- src/main/java/net/gfu/seminar/spring/lab/todoapp/dao/TodoItemDao.java
+++ src/main/java/net/gfu/seminar/spring/lab/todoapp/dao/TodoItemDao.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import net.gfu.seminar.spring.lab.todoapp.domain.TodoItem;
@@ -15,14 +16,31 @@ public class TodoItemDao {
 	@PersistenceContext
 	private EntityManager em;
 	
+	/**
+	 * Als Kommentar in dieser Methode Pseudocode, der das Verhalten von @Transcational verdeutlicht  
+	 */
 	public void insert(TodoItem item) {
+		// tx = lookup existing Transaction
+		// if tx == null
+		//  tx = new Transaction
+		//  iAmCreator = true
+		//  try {
 		em.persist(item);
+		//  } catch (Exception e) {
+		//    tx.setRollbackOnly
+		//    throw e
+		//  } finally {
+		//    if iAmCreator
+		//         tx.close
+		// }
 	}
 	
 	public TodoItem update(TodoItem item) {
 		return em.merge(item);
 	}
 	
+	// So würde man eine von der Klassendefinition abweichende Transaktionsdefinition für die Methode machen
+	// @Transactional(propagation = Propagation.REQUIRES_NEW)
 	public TodoItem findById(Integer id) {
 		return em.find(TodoItem.class, id);
 	}
