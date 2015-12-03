31a60795f50cc6800604821399479e86fed63600
Rene Gielen <rgielen@apache.org>
2015-12-03 22:22:33 +0100

    Profil Konfiguration in Test

--- src/test/java/net/gfu/seminar/spring/lab/todoapp/dao/TodoItemDaoTest.java
+++ src/test/java/net/gfu/seminar/spring/lab/todoapp/dao/TodoItemDaoTest.java
@@ -1,25 +1,29 @@
 package net.gfu.seminar.spring.lab.todoapp.dao;
 
-import static org.junit.Assert.fail;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import net.gfu.seminar.spring.lab.todoapp.TodoAppConfig;
 import net.gfu.seminar.spring.lab.todoapp.domain.TodoItem;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {TodoAppConfig.class})
+@ActiveProfiles({"test"})
 public class TodoItemDaoTest {
 
 	@Autowired
 	TodoItemDao dao;
 	
+	@Autowired
+	private Environment env;
+
 	@Test
 	public void testThatSavedItemIsFoundById() {
 		TodoItem item = new TodoItem("foo", "bar");
